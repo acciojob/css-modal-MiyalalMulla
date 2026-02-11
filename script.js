bbcba32282c35265b070af33fcde887313bcb510
MiyalalMulla <mullamiyalal@gmail.com>
2026-02-11 13:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const openBtn = document.getElementById("openModal");
+    const modal = document.getElementById("modal");
+    const closeBtn = document.querySelector(".close-modal");
+
+    openBtn.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    window.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
